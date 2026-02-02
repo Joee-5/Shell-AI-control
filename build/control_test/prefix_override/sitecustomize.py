78f88b09cce0042daec28ae75cd7096ed2b732ab
Joee-5 <youssefysa@yahoo.com>
2026-02-02 11:34:45 +0200

    Removed kinematic bicylce & car_controller packages

--- build/control_test/prefix_override/sitecustomize.py
+++ build/control_test/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/omar/shellsim/Shell-AI-control/install/control_test'
+    sys.prefix = sys.exec_prefix = '/home/joee-5/ros2_ws/install/control_test'
